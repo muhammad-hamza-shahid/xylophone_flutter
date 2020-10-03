8a54144b3d03833519d724041b4e9c8c0386c9df
muhammad-hamza-shahid <50265613+muhammad-hamza-shahid@users.noreply.github.com>
2020-10-04 01:30:00 +0500

    added sounds and edited buttons and code

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:random_words/random_words.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  void playSound(int a) {
+    final player = AudioCache();
+    player.play('note$a.wav');
+  }
+
+  Expanded buildButton(int a, Color color) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(a);
+        },
+      ),
+    );
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold());
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildButton(1, Colors.red.shade700),
+              buildButton(2, Colors.deepOrange.shade400),
+              buildButton(3, Colors.yellow),
+              buildButton(4, Colors.purple),
+              buildButton(5, Colors.blue.shade900),
+              buildButton(6, Colors.blue),
+              buildButton(7, Colors.lightBlueAccent),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
